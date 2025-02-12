601f1bbd1d838487ce66e96e49a0f6360c57d23f
VishalBala@Office-Jeevijay <vishal.bala.100@gmail.com>
2025-02-12 14:08:14 +0530

    fix

--- src/components/blog/BlogPageTwo.jsx
+++ src/components/blog/BlogPageTwo.jsx
@@ -39,13 +39,7 @@ function BlogPageTwo({ countryName, id }) {
       </div>
     );
   }
-  // if (countryName && blogData.length < 1) {
-  //   return (
-  //     <div className="">
-  //       <GridLoaderSpinner />
-  //     </div>
-  //   );
-  // }
+
 
   return (
     <div className="max-w-4xl mx-auto p-4 sm:p-6 bg-white shadow-lg rounded-lg">
